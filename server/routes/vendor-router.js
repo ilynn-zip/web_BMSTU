c6f8d03919a1890a24334dad73c7f330ccdaa5c2
Danon12n <masterwars22@yandex.ru>
2023-02-12 22:31:26 +0300

    upd
    bug fix

--- server/routes/vendor-router.js
+++ server/routes/vendor-router.js
@@ -4,6 +4,7 @@ const vendorController = require("../controllers/vendor-controller");
 
 router.delete("/deletePet", vendorController.deletePet);
 
+router.post("/createPet", vendorController.createPet);
 router.post("/updatePet", vendorController.updatePet);
 router.post("/getShopOrders", vendorController.getShopOrders);
 router.post("/getShopPets", vendorController.getShopPets);
