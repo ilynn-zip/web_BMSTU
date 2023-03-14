9481da1f5a1595d711075f196ca2bb1352660729
Riseofbattleaxe <riseofbattleaxe@gmail.com>
2023-03-14 13:17:24 +0300

    Update filter.tsx

--- client/src/components/filter/filter.tsx
+++ client/src/components/filter/filter.tsx
@@ -86,7 +86,7 @@ const Filter: FC<FilterProps> = () => {
             onSubmit={onFormSubmit}
             className={styles.filterWrapper}
         >
-            <p className={styles.filterTitle}>Новый Фильтр</p>
+            <p className={styles.filterTitle}>Фильтр1</p>
             <MyDropMenu
                 id='petType'
                 options={["Any", "Cat", "Dog", "Hedgehog", "Raccoon", "Fox"]}
